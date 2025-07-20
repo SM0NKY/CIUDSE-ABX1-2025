cc3b963cfbe70231cb6d8af91c4de5c395208a8f
Doblepapa102 <skrunfer@gmail.com>
2025-07-19 23:21:55 -0600

    Update GPS.py

--- GPS.py
+++ GPS.py
@@ -1,24 +1,39 @@
+#Esto es para que sea fluido pero de igual manera hacer correcciones en dado caso de no necesitarse
+import time
+timestamp = time.time()
+import matplotlib.pyplot as plt
+
+#Visualización en tiempo real (matplotlib o folium)
+plt.plot(1,2,3,4,5,6) #ejemplo
+plt.title("Trayectoria hoizontal")
+plt.ylabel("Tiempo")
+plt.xlabel("Desplazamiento")
+plt.show()
 class Datos:
     pass
 class Correccion:
     pass
 
 class Datos_Pos(Datos):
-    def Datos_Ad(self, Data_Posi):
+    def __init__(self):
+        self.data_pos = 0.0  # posición inicial
+
+    def datos_adquiridos(self, data_posi):
         """
-        Estos son los datos que se adqieren segun laa posicion horizontal
+        Guarda los datos adquiridos de posición horizontal
         """
-        self.Data_Pos = Data_Posi
-        def Obtenerer_Da(self):
-            return self.Data_posi
+        self.data_pos = data_posi
+
+    def obtener_dato(self):
+        return self.data_pos
         
 class Correc(Correccion):
     def Correc_Data(self, Datos:Datos_Pos):
         """
         Esto es para la correccion de la posicion
         """
-        Posicion = self.obtener_Data()
-        if Posicion >= 5 | Posicion <= -5:
+        Posicion = Datos.Obtener_Datos()
+        if Posicion >= 5 or Posicion <= -5:
             print("Se requiere de correcciones")
             return True
         else:
@@ -26,3 +41,13 @@ class Correc(Correccion):
             return False
         
         #Todo este codigo es solo para hacer las correcciones horizontales
+
+
+
+        #Esto es para probar sin un GPS
+        """
+        datos = [0, 2, 4, 6, 8, 10]
+for d in datos:
+    pos.datos_adquiridos(d)
+    correc.correc_data(pos)
+        """
